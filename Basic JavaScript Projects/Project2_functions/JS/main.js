15580ee072909f94cf6ef5000b95e86a42c0ec8b
Dwalters10 <154628764+Dwalters10@users.noreply.github.com>
2024-01-18 08:02:53 -0700

    Project2 Index.html and main.js
    Project 2 functions assignment.

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+// Basic Function
+function My_First_Funtion() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+// += operator
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
