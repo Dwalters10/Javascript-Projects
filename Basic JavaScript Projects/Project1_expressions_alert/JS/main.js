a44e17da1b6432bb086e77a8df742ec98be739d0
Dwalters10 <154628764+Dwalters10@users.noreply.github.com>
2024-01-18 07:31:20 -0700

    index and Main.js created
    Created Index.html and main.js added to Project1_expressions_alert folder and completed Basic JavaScript Projects Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,39 @@
+
+// main.js
+
+// window alert
+window.alert("Hello World");
+
+// document.write method
+document.write('Hello, World!');
+
+
+// displaying a variable
+var A = "This is a string";  
+document.write(A); 
+
+// Window alert using a variable
+var B = "String Assignemt";
+window.alert(B);
+
+// how to use \ in text to display \ and ""
+document.write("This is to test the \\ so I can display \" in a line of text" ); 
+
+// Concatenated Strings
+document.write("\"Be who you are and say what you feel," 
++ "because those who mind don\'t matter and those who matter dont\'t mind.\""
++ "-Dr. seuss");
+
+// Concatenated Variable
+var C = "Concatenated" + " String" 
+document.write(C)
+
+// creating Multiple Varriables
+var sent1 = "This is the beginning of the string", sent2 = " and this is the end of the string"; 
+
+// displaying the Variables
+document.write(sent1+sent2);
+
+// an expression
+document.write(48*3)
+
